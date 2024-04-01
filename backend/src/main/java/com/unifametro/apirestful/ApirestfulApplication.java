c88d97165790ba5897b030a25dbb1665367db396
miguelsoares01 <miguelsoares01@gmail.com>
2024-04-01 16:07:18 -0300

    sinc-inicial

---
+++ backend/src/main/java/com/unifametro/apirestful/ApirestfulApplication.java
@@ -0,0 +1,13 @@
+package com.unifametro.apirestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestfulApplication.class, args);
+	}
+
+}
