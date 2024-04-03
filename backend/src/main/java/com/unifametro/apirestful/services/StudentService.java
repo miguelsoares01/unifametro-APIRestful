07cc254b37d8d3f15e9af602579bab26487dfcd5
miguelsoares01 <miguelsoares01@gmail.com>
2024-04-03 20:52:10 -0300

    criando StudentService

--- backend/src/main/java/com/unifametro/apirestful/services/StudentService.java
+++ backend/src/main/java/com/unifametro/apirestful/services/StudentService.java
@@ -1,5 +1,25 @@
 package com.unifametro.apirestful.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.unifametro.apirestful.entities.Student;
+import com.unifametro.apirestful.repositories.StudentRepository;
+
+@Service
 public class StudentService {
- @service
+	
+	@Autowired
+	private StudentRepository repository;
+	
+	
+	@Transactional(readOnly = true)
+	public List<Student> findAll(){
+		
+		return repository.findAll();
+	}
+ 
 }
